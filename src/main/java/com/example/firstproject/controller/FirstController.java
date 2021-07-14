1ca76f4c4ff3147440c7dc4a156315ee542609c2
sdcomms4227 <sdcomms4227@gmail.com>
2021-07-14 18:44:30 +0900

    initialize

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,23 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "센스");
+        return "greetings"; // templates/greetings.mustache -> 브라우저로 전송!
+   }
+
+   @GetMapping("/bye")
+   public String seeYouNext(Model model){
+        model.addAttribute("username", "센스");
+        return "goodbye";
+   }
+}
+
+
